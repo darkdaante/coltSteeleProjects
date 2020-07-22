4dac08034b01fbaa59e4952d13ce618a1c7f4828
darkdaante <45766636+darkdaante@users.noreply.github.com>
2020-07-22 15:50:48 +0530

    Add files via upload

---
+++ YelpCamp/v5/app.js
@@ -0,0 +1,101 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var Campground = require('./models/campground');
+var seedDB = require('./seeds');
+var Comment = require('./models/comment');
+
+mongoose.connect('mongodb://localhost:27017/yelp_camp_v5', { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+// seedDB();
+
+app.get('/', function(req, res) {
+	res.render('landing');
+});
+
+app.get('/campgrounds', function(req, res) {
+	Campground.find({}, function(err, allcampgrounds) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.render('campgrounds/index', { campgrounds: allcampgrounds });
+		}
+	});
+});
+
+app.get('/campgrounds/new', function(req, res) {
+	res.render('campgrounds/new');
+});
+
+app.post('/campgrounds', function(req, res) {
+	var newname = req.body.name;
+	var newimage = req.body.image;
+	var newdescription = req.body.description;
+	var newCampground = { name: newname, image: newimage, description: newdescription };
+	Campground.create(newCampground, function(err, campground) {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log('New Campground Added');
+			console.log(Campground);
+			res.redirect('/campgrounds');
+		}
+	});
+});
+
+app.get('/campgrounds/:id', function(req, res) {
+	Campground.findById(req.params.id).populate('comments').exec(function(err, foundCampground) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.render('campgrounds/show', { campground: foundCampground });
+		}
+	});
+});
+
+// =========================
+//      COMMENTS ROUTES
+// =========================
+
+app.get('/campgrounds/:id/comments/new', function(req, res) {
+	//find campground by id
+	Campground.findById(req.params.id, function(err, foundcampground) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.render('comments/new', { campground: foundcampground });
+		}
+	});
+});
+
+app.post('/campgrounds/:id/comments', function(req, res) {
+	//lookup campground using id
+	Campground.findById(req.params.id, function(err, campground) {
+		if (err) {
+			console.log(err);
+			res.redirect('/campgrounds');
+		} else {
+			Comment.create(req.body.comment, function(err, comment) {
+				if (err) {
+					console.log(err);
+				} else {
+					campground.comments.push(comment);
+					campground.save();
+					res.redirect('/campgrounds/' + campground._id);
+				}
+			});
+		}
+	});
+	//create new comment
+	//connect new comment to campground
+	//redirect campground to show page
+});
+
+app.listen(3000, function() {
+	console.log('YelpCamp Server Has Started');
+});
---
+++ YelpCamp/v7/readme.md
@@ -0,0 +1,121 @@
+#YelpCamp Initial
+
+* Add Landing Page
+* Add Campground Page that Lists all Campground 
+
+Each CampGround has:
+* Name
+* Image
+
+
+
+#Layout and Basic Styling
+* Create our header and footer partials
+* Add in Bootstrap
+
+
+
+#Creating New Campgrounds
+* Setup new campground POST route
+* Add in body-parser
+* Setup route to show form
+* Add basic unstyled form
+
+
+
+#Style the Campgrounds page
+* Add a better header/title
+* Make Campgrounds display in a grid
+
+
+
+#Style the Navbar and Form
+* Add a navbar to all templates
+* Style the new Campground Form
+
+
+
+#Adding Mongoose
+* Install and configure mongoose
+* Setup campground model
+* Use Campground model inside of our routes!
+
+
+
+#Show Page
+* Review the RESTful routes we've seen so far
+* Add description to our campground model
+* Show db.collection.drop()
+* Add a show route/template
+
+
+
+#Refactor Mongoose Code
+* Create a models directory
+* Use module.exports
+* Require everything correctly
+
+
+
+#Add Seeds File
+* Add a seeds.js file
+* Run the seeds file every time the server starts
+
+
+
+#Add the Comment model!
+* Make our errors go away!
+* Display comments on campground show page
+
+
+
+#Comment New/Create
+* Discuss nested routes
+* Add the comment new and create routes
+* Add the new Comment form
+
+
+
+#Style Show Page
+* Add sidebar to show page
+* Display Comments nicely
+
+
+
+#Finish Styling Show Page
+* Add Public directory
+* Add Custom Stylesheet
+
+
+
+#Auth P1 - Add User Model
+* Install All packages needed for auth
+* Define User Model
+
+#Auth P2 - Register
+* Configure Passport
+* Add register routes
+* Add register template
+
+
+
+#Auth P3 - Login
+* Add login routes
+* Add login template
+
+
+
+#Auth P4 - Logout/NavBar
+* Add logout route
+* Prevent user from adding a comment if not signed in
+* Add links to navbar
+
+
+
+#Auth P5 - Show/Hide Links
+* Show/hide auth links in navbar correctly
+
+
+
+#Refactor The Routes
+* Use Express router to recognize all routes
